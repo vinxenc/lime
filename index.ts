21ca5b9a821b94dcf48fa8092ca942f60b89a8ff
Vinxenc <mac@Vinxenc.local>
2021-03-22 22:32:33 +0700

    feature: setup react client

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 import * as express from 'express';
 import * as ParseDashboard from 'parse-dashboard';
+import * as path from 'path';
 import { ParseServer } from 'parse-server';
 import { 
     PARSE_SERVER_PROPERTY, 
     PARSE_DASHBOARD_PROPERTY, 
     PARSE_DASHBOARD_OPTIONS 
-} from '../settings/parse';
+} from './settings/parse';
 
 const app = express();
 
-app.get('/', function (_: express.Request, res: express.Response) {
-    res.send('Permission denied')
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', function (_: express.Request, res: express.Response) {
+//     res.send('Permission denied')
+// })
 
 const api = new ParseServer(PARSE_SERVER_PROPERTY);
 const dashboard = new ParseDashboard(
@@ -19,13 +22,15 @@ const dashboard = new ParseDashboard(
     PARSE_DASHBOARD_OPTIONS
 );
 
-console.log(PARSE_DASHBOARD_PROPERTY, PARSE_DASHBOARD_OPTIONS)
-
 // Serve the Parse API on the /parse URL prefix
 app.use('/api', api);
 
 // make the Parse Dashboard available at /dashboard
 app.use('/dashboard', dashboard);
+
+app.use('*', (_: express.Request, res: express.Response) => res.sendFile(
+    __dirname + '/public/index.html')
+);
 
 app.listen(process.env.PORT || 1337, function () {
     console.log('parse-server-example running on port 1337.');

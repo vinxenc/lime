8f9871877b5cb50a11bfb341d0dc3b47a08229d6
Vinxenc <mac@Vinxenc.local>
2021-03-29 22:10:13 +0700

    feature: split code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 import * as React from 'react';
+import * as Loadable from 'react-loadable';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
+const Loading = function() {
+    return <div className="pt-3 text-center">
+        <div className="sk-spinner sk-spinner-pulse">Loang...</div>
+    </div>
+}
+    
+// Pages
+const Login = Loadable({
+    loader: () => import('./pages/login/Login'),
+    loading: Loading,
+    delay: 2000,
+    timeout: 10000,
+});
+const Error = Loadable({
+    loader: () => import('./pages/error/Error'),
+    loading: Loading,
+    delay: 2000,
+    timeout: 10000,
+});
+// const Login = React.lazy(() => import('./pages/login/Login'));
 
 class App extends React.Component {
     render() {
@@ -13,18 +35,26 @@ class App extends React.Component {
                         <li>
                             <Link to="/login">Login</Link>
                         </li>
+                        <li>
+                            <Link to="/error">Error</Link>
+                        </li>
                         {/* <li>
                             <Link to="/dashboard">Dashboard</Link>
                         </li> */}
                     </ul>
                 </div>
-                <Switch>
-                    <Route exact path="/login" render={() => <div>Login</div>} />
-                    {/* <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
-                    <Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
-                    <Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} /> */}
-                    <Route path="/" render={() => <div>Home</div>} />
-                </Switch>
+                <React.Suspense fallback={Loading}>
+                    <Switch>
+                        {/* <Route exact path="/login" render={() => <div>Login</div>} /> */}
+                        {/* <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
+                        <Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
+                        <Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} /> */}
+                        <Route path="/login" component={props => <Login {...props}/>} />
+                        <Route path="/error" component={props => <Error {...props}/>} />
+                        <Route path="/" render={() => <div>Home</div>} />
+                    </Switch>
+                </React.Suspense>
+                
             </BrowserRouter>
         );
     }
---
+++ src/pages/error/Error.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Error = () => {
+    return (
+        <div className="error">
+            <h1>Error</h1>
+            <p className="text-muted">Error Not Found</p>
+        </div>                
+    );
+};
+
+export default Error;

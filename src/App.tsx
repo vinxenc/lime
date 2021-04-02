aade8dce97f4e1276eef1079e989b5e139dcb09d
Vinxenc <mac@Vinxenc.local>
2021-04-02 16:38:37 +0700

    feature: structure code FE

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import * as React from 'react';
-import * as Loadable from 'react-loadable';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import routes from './routes';
 
-const Loading = function() {
-    return <div className="pt-3 text-center">
-        <div className="sk-spinner sk-spinner-pulse">Loang...</div>
-    </div>
-}
+import Loading from './components/loading/Loading';
+
+// const Loading = function() {
+//     return <div className="pt-3 text-center">
+//         <div className="sk-spinner sk-spinner-pulse">Loang...</div>
+//     </div>
+// }
     
 // Pages
-const Login = Loadable({
-    loader: () => import('./pages/login/Login'),
-    loading: Loading,
-    delay: 2000,
-    timeout: 10000,
-});
-const Error = Loadable({
-    loader: () => import('./pages/error/Error'),
-    loading: Loading,
-    delay: 2000,
-    timeout: 10000,
-});
+// const Login = Loadable({
+//     loader: () => import('./pages/login/Login'),
+//     loading: Loading,
+//     delay: 2000,
+//     timeout: 10000,
+// });
+// const Error = Loadable({
+//     loader: () => import('./pages/error/Error'),
+//     loading: Loading,
+//     delay: 2000,
+//     timeout: 10000,
+// });
 // const Login = React.lazy(() => import('./pages/login/Login'));
 
 class App extends React.Component {
@@ -49,9 +51,20 @@ class App extends React.Component {
                         {/* <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
                         <Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
                         <Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} /> */}
-                        <Route path="/login" component={props => <Login {...props}/>} />
+                        {/* <Route path="/login" component={props => <Login {...props}/>} />
                         <Route path="/error" component={props => <Error {...props}/>} />
-                        <Route path="/" render={() => <div>Home</div>} />
+                        <Route path="/" render={() => <div>Home</div>} /> */}
+                        {routes.map((route, idx) => {
+                            return route.component && (
+                                <Route
+                                key={idx}
+                                path={route.path}
+                                exact={route.exact}
+                                component={props => (
+                                    <route.component {...props} /> 
+                                )} />
+                            )
+                        })}
                     </Switch>
                 </React.Suspense>
                 
